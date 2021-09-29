f3f965b41521a2c215e2dabbe9a9015856670fec
Michael Good <mjgood@coastal.edu>
2021-09-29 00:42:36 -0400

    added testcases

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -18,25 +18,7 @@ namespace Models
 
         public int Id{get;set;}
         public string Name{get;set;}
-        private long _PhoneNum;
-        public long PhoneNum
-        {
-            get
-            {
-                return PhoneNum;
-            }
-            set
-            {
-                if(value > 9999999999 || value < 1000000000)
-                {
-                    throw new InputInvalidException("Rating must be a valid");
-                }
-                else
-                {
-                    _PhoneNum = value;
-                }
-            }
-        }
+        public long PhoneNum{get;set;}
 
     }
 }
--- Tests/ModelTests.cs
+++ Tests/ModelTests.cs
@@ -22,6 +22,21 @@ namespace Tests
         }
 
         [Fact]
+        public void CustomerShouldSetValidId()
+        {
+            //Arrange
+            Customer test = new Customer();
+            int testId = 123;
+
+            //Act
+            test.Id = testId;
+
+            //Assert
+            Assert.Equal(testId, test.Id);
+
+        }
+
+        [Fact]
         public void CustomerShouldSetValidPhoneNum()
         {
             //Arrange
@@ -48,16 +63,89 @@ namespace Tests
             //Assert
             Assert.Equal(testAddress, test.Address);
         }
-
-        [Theory]
-        [InlineData(-1)]
-        [InlineData(10000000000)]
-        [InlineData(10000)]
-        public void CustomerShouldNotSetInvalidPhoneNum(long input)
+        
+        [Fact]
+        public void StoreFrontShouldSetValidId()
         {
-            Customer test = new Customer();
+             //Arrange
+            StoreFront test = new StoreFront();
+            int testId = 123;
 
-            Assert.Throws<InputInvalidException>(() => test.PhoneNum = input);
+            //Act
+            test.Id = testId;
+
+            //Assert
+            Assert.Equal(testId, test.Id);
+        }
+
+        [Fact]
+        public void OrderShouldSetValidId()
+        {
+             //Arrange
+            Order test = new Order();
+            int testId = 123;
+
+            //Act
+            test.Id = testId;
+
+            //Assert
+            Assert.Equal(testId, test.Id);
+        }
+
+        [Fact]
+        public void OrderShouldSetValidTotal()
+        {
+             //Arrange
+            Order test = new Order();
+            decimal testTotal = (decimal)12.5400;
+
+            //Act
+            test.Total = testTotal;
+
+            //Assert
+            Assert.Equal(testTotal, test.Total);
+        }
+
+        [Fact]
+        public void OrderShouldSetValidAdress()
+        {
+             //Arrange
+            Order test = new Order();
+            string testAddress = "693 Liberty StreetProvidence, RI 02904";
+
+            //Act
+            test.StoreAddress = testAddress;
+
+            //Assert
+            Assert.Equal(testAddress, test.StoreAddress);
+        }
+
+        [Fact]
+        public void OrderShouldSetValidDate()
+        {
+             //Arrange
+            Order test = new Order();
+            string testDate = "9/28/2021 12:00:00 AM";
+
+            //Act
+            test.DateOfOrder = testDate;
+
+            //Assert
+            Assert.Equal(testDate, test.DateOfOrder);
+        }
+
+        [Fact]
+        public void ProductShouldSetValidPrice()
+        {
+             //Arrange
+            Product test = new Product();
+            decimal testPrice = (decimal)19.42;
+
+            //Act
+            test.Price = testPrice;
+
+            //Assert
+            Assert.Equal(testPrice, test.Price);
         }
     }
 }
